12cdb6f71a333843e6f3067b7fb1c318098e828e
opilane <opilane@HP-CZC2349HTS>
2023-02-01 14:50:07 +0200

    Add project files.

---
+++ Isikukood.py
@@ -0,0 +1,17 @@
+﻿from module1 import *
+ikood=""
+while True:
+    ikood=input("Sisesta isikood: ")
+    if pikkus(ikood)==False:
+        print("Liiga pikk või lühike isikukood")
+    else:
+        s=sugukontroll(ikood)
+        if s=="viga":
+            print("Eesimene täht ei ole õige")
+        else:
+            print(s)
+            if sünnipäev(ikood)=="viga":
+                print("2-7 tähed on valesti sisetatud")
+            else:
+                lause(ikood)
+              
