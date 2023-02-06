f2f9084bcf75c219160869858b186b62317f3dc9
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2023-02-06 14:24:14 +0200

    Update module1.py

--- module1.py
+++ module1.py
@@ -1,4 +1,7 @@
-﻿def pikkus(ikood:str)->bool:
+from re import S
+
+
+def pikkus(ikood:str)->bool:
     """Funktsioon tagastab True, kui pikkus on 11 sümbolid
     param str ikood: Inimesed isikukood
     rtype: bool    
@@ -79,3 +82,47 @@ def sünnikoht(ikood: str)->str:
 
 def lause(ikood: str)->str:
     print(f"See on {sugukontroll(ikood)} ta on sündinud {sünnipäev(ikood)}, tema sünnikoht on {sünnikoht(ikood)}")
+
+
+def kontrollnr(ikood:str)->int:
+    astme1=[1,2,3,4,5,6,7,8,9,1]
+    astme2=[1,2,3,4,5,6,7,8,9,1,2,3]
+    ik_list=list(ikood)
+    ik_list=list(map(int,ik_list))
+    summa=0
+    for i in range(0,10,1):
+        summa+=ik_list[i]*astme1[i]
+    s=(summa//11)*11
+    jaak=summa-s
+    if jaak==int(ik_list[10]):
+        return jaak
+    elif jaak==10:
+        return 0
+    else:
+        summa=0
+        for i in range(0,10,1):
+            summa+=ik_list[i]*astme2[i]
+        s=(summa//11)*11
+        jaak=summa-s
+        return jaak
+
+
+
+def naised_mehed(ikoodid:list)->list:
+    naised=[] 
+    mehed=[] 
+    for kood in ikoodid:
+        kood=list(kood) 
+        if int(kood[0])%2==0:
+            naised.append(kood)
+        else:
+            mehed.append(kood)
+    naised.extend(mehed)
+    ikoodid.clear()
+    ikoodid=naised
+    return ikoodid
+
+def arvud_sorted(arvud:list)->list:
+    arvud=list(map(int,arvud)) #[111,3333,2222,3444,222,7]
+    arvud.sort()
+    return arvud
