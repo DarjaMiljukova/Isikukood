9212a9525428e41495397f14f6425de27cdba0ed
DarjaMiljukova <120181585+DarjaMiljukova@users.noreply.github.com>
2023-02-06 14:23:18 +0200

    Update Isikukood.py

--- Isikukood.py
+++ Isikukood.py
@@ -1,17 +1,32 @@
-﻿from module1 import *
+from module1 import *
 ikood=""
+arvud=["100","1001","211","222"]
+ikoodid=[]
 while True:
-    ikood=input("Sisesta isikood: ")
-    if pikkus(ikood)==False:
-        print("Liiga pikk või lühike isikukood")
-    else:
-        s=sugukontroll(ikood)
-        if s=="viga":
-            print("Eesimene täht ei ole õige")
+    a=input("Kas sa tahad jätkata? jah=ja ei=ei :  ")
+    if a=="ja":
+        ikood=input("Sisesta isikood: ")
+        if ikood=="-":break
+        if pikkus(ikood)==False:
+            print("Liiga pikk või lühike isikukood")
+            arvud.append(ikood)
         else:
-            print(s)
-            if sünnipäev(ikood)=="viga":
-                print("2-7 tähed on valesti sisetatud")
+            s=sugukontroll(ikood)
+            if s=="viga":
+                print("Eesimene täht ei ole õige")
             else:
-                lause(ikood)
-              
+                print(s)
+                if sünnipäev(ikood)=="viga":
+                    print("2-7 tähed on valesti sisetatud")
+                else:
+                    print(lause(ikood))
+                    if kontrollnr(ikood)==int(ikood[-1]):
+                        print("Ok")
+                        ikoodid.append(ikood)
+                    else:
+                        print("!!!")
+print()
+ikoodid=naised_mehed(ikoodid) 
+print(ikoodid) 
+arvud=arvud_sorted(arvud)
+print(arvud)
